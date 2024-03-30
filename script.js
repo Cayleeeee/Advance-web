946fabda280ea363192c07c1c2e2566cff04e4a5
Cayleeeee <155513661+Cayleeeee@users.noreply.github.com>
2024-03-30 23:25:25 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showMenu(menuId) {
-    var modal = document.getElementById(menuId);
-    modal.style.display = "block";
-}
-
-function closeMenu(menuId) {
-    var modal = document.getElementById(menuId);
-    modal.style.display = "none";
-}
