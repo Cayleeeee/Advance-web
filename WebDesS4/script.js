656ce6b512d6ab43f9714a327426af1ed6dfa9c0
Cayleeeee <155513661+Cayleeeee@users.noreply.github.com>
2024-03-30 23:27:43 +0800

    Add files via upload

---
+++ WebDesS4/script.js
@@ -0,0 +1,9 @@
+function showMenu(menuId) {
+    var modal = document.getElementById(menuId);
+    modal.style.display = "block";
+}
+
+function closeMenu(menuId) {
+    var modal = document.getElementById(menuId);
+    modal.style.display = "none";
+}
